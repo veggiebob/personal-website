11ab8ece0a2bf67b3c7b0a7fd3521f0242eab5ae
veggiebob <veggiebobowman@gmail.com>
2022-01-31 00:38:14 -0500

    add parse script

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,15 @@
-var sendMessage = function(message) {
-    fetch('/', {
-        method: 'PUT',
+function sendMessage (message) {
+    console.log('new!');
+    fetch('parse', {
+        method: 'POST',
         body: message,
         headers: {
             'Content-Type': 'text/plain',
             'Content-Length': message.length
-        },
-        version: 'HTTP/6.9'
+        }
     }).then(res => {
         console.log(res);
     }).catch(err => {
         console.log(err);
     })
-}
-
-const form = document.querySelector('form');
-console.log(form );
-form.onsubmit = (e) => {
-    e.preventDefault();
-    console.log(e.target[0].value);
-    sendMessage(e.target[0].value);
 }
