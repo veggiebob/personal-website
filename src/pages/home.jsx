c8fa2f7a908bb4020f8210c1046bad969af893ca
veggiebob <veggiebobowman@gmail.com>
2022-03-21 23:18:32 -0400

    add comma

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,7 +6,7 @@ const MainPage = () => (
     <h1 className="text-5xl leading-none font-thin ">Hi, I'm Andrew!</h1>
     <h2 className="text-xl">
       I'm an undergraduate Computer Science student at RIT, with an interest in
-      Rust Haskell, and JavaScript.
+      Rust, Python, Haskell, and JavaScript.
       <div className="h-2" />
       Here are some of my favorite projects I am and have been working on:
     </h2>
