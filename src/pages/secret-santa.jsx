0feb4551ab6b858c2b75b76fb20ffa3ced6b27d2
veggiebob <veggiebobowman@gmail.com>
2022-11-09 20:19:12 -0500

    add secret santa page

---
+++ src/pages/secret-santa.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { CONFIG } from "../runconfig";
+
+const SecretSanta = () => {
+    let [selected, setSelected] = useState(false);
+    let [name, setName] = useState("awaiting response...");
+    let [options, setOptions] = useState(null);
+    let [err, setErrorMessage] = useState("");
+    if (options === null) { 
+        fetch(
+            CONFIG.serverLocation + '/secret-santa/options',
+            {
+                headers: {
+                    'Access-Control-Allow-Headers': '*'
+                }
+            }
+        ).then(response => {
+            response.json().then(data => {
+                setOptions(data)
+            }).catch(e => {
+                console.log("Failed to parse json! See error below:");
+                console.log(e);
+                setErrorMessage('Bad formatting of JSON')
+            })
+        }).catch(e => {
+            console.log("failed to fetch secret santa options from server");
+            setOptions([])
+            setErrorMessage('Could not fetch options')
+        })
+    }
+    const getName = name => {
+        setSelected(true);
+        fetch(
+            CONFIG.serverLocation + '/secret-santa/giftee/' + name,
+            {
+                headers: {
+                    'Access-Control-Allow-Headers': '*'
+                }
+            }
+        ).then(response => {
+            response.text().then(text => {
+                setName(text);
+            }).catch(e => {
+                setErrorMessage("Unable to read text from response");
+            })
+        }).catch(e => {
+            console.log("Could not fetch result from server");
+            setErrorMessage('Could not fetch secret santa result')
+        })
+    }
+    let optionsButtons = options === null || options.length === 0 ? 
+        <span>no options</span> : 
+        <div>
+            {options.map(
+                n => <div key={n}>
+                        <button type="button" 
+                            onClick={e => getName(n)}>{n}</button>
+                            <br/>
+                    </div>)}
+        </div>
+    return <div>
+        {selected ? <span>{name}</span> : optionsButtons}
+                <br />
+                <span className="text-red-600">{err}</span>
+    </div>
+}
+
+export default SecretSanta;
