d2c049e8855204c7d9eabf1c61bb7c36d59729c3
veggiebob <veggiebobowman@gmail.com>
2023-12-04 16:51:35 -0500

    change name in footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="border-t-[1px] p-4 border-neutral-200 text-center text-sm">
-      Designed by <a href="https://github.com/veggiebob">Andrew</a> and{" "}
+      Designed by <a href="https://github.com/veggiebob">Jacob</a> and{" "}
       <a href="https://github.com/chrispittman343">Chris</a>
     </footer>
   );
